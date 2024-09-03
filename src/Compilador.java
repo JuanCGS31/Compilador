d176504d2614047af58197a20f23e43ef1a717f5
Ana Gracia <ana.gracia@cetys.edu.mx>
2024-09-02 18:15:57 -0700

    Lineas de codigo

--- src/Compilador.java
+++ src/Compilador.java
@@ -1,9 +1,18 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.Timer;
+import compilerTools.Directory;
+import compilerTools.ErrorLSSL;
+import compilerTools.Production;
+import compilerTools.TextColor;
+import compilerTools.Token;
+
 public class Compilador {
     private String title;
-    private Directory directorio;
-    private ArrayList<token> tokens; //lista de tokens para el lexico
+    private Directory directorio; //creo que no se va a usar
+    private ArrayList<Token> tokens; //lista de tokens para el lexico
     private ArrayList<ErrorLSSL> errors; //lista de errores encontrados
     private ArrayList<TextColor> textColors; //para darle color a las palabras reservadas
     private Timer timerKeyReleased;
@@ -12,7 +21,13 @@ public class Compilador {
     private boolean compiledSuccess = false;
 
     public Compilador() {
+        initComponents();
+        init();
+    }
 
+    private void init() {
+        title = "C en Español";
+        
     }
 
 }
