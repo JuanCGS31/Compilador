62f2a29d80a7ebff7b40bdb36e06dbdec32e63cb
Ana Gracia <ana.gracia@cetys.edu.mx>
2024-09-02 17:59:16 -0700

    test

--- Compilador.java
+++
@@ -1,3 +0,0 @@
-public static void main(String[] args) {
-    //int juan=0, ana=1;
-}
--- src/Compilador.java
+++ src/Compilador.java
@@ -5,9 +5,9 @@ public class Compilador {
     private Directory directorio;
     private ArrayList<token> tokens; //lista de tokens para el lexico
     private ArrayList<ErrorLSSL> errors; //lista de errores encontrados
-    private ArrayList<TextColor> textColors;
+    private ArrayList<TextColor> textColors; //para darle color a las palabras reservadas
     private Timer timerKeyReleased;
-    private ArrayList<Production> idenProd;
+    private ArrayList<Production> idenProd; 
     private HashMap<String, String> identificadores;
     private boolean compiledSuccess = false;
 
